2e47f2c581f62d83d5d2256662eff3e61747bdf6
Pierre CHARLES <pier.charles@hotmail.fr>
2016-07-20 21:58:28 +0200

    Combining ReZurection Phaser game with ionic blank app

---
+++ www/js/main.js
@@ -0,0 +1,14 @@
+﻿"use strict";
+
+var game;
+
+
+window.onload = function() {
+
+    game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO);
+
+    game.state.add('main_screen',  MainScreen);
+    game.state.start('main_screen');
+};
+
+
